40c378eb1e80343420105aa5edd813ca11c2e2ea
Christopher Wall <wallandall@gmail.com>
2017-11-05 14:40:50 +0100

    Controller

---
+++ _config.py
@@ -0,0 +1,12 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'y\x80\xd5\xe4MLt~3\x90\xda\ro\xf2\xdf\xb6\xd1\xbd\xca\xc5\xe2j\xcb\x80'
+
+
+DATABASE_PATH = os.path.join(basedir, DATABASE)
